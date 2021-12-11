a063c1b82919f725f40a306532d31c0b90af9f17
Eduard Draghiciu <eduarddr96@gmail.com>
2021-12-11 11:44:49 -0500

    add dependencies

--- screens/AddNewEntry.js
+++ screens/AddNewEntry.js
@@ -6,6 +6,7 @@ import {
 	TextInput,
 	Button,
 	SafeAreaView,
+	ScrollView,
 } from 'react-native';
 import CameraUI from './Camera';
 import { Camera } from 'expo-camera';
@@ -26,6 +27,9 @@ export default function AddNewEntry({ navigation }) {
 	const handleOpenCamera = () => {
 		setIsCameraOpen(true);
 	};
+
+	const handleGetLocation = () => {};
+
 	return (
 		<SafeAreaView>
 			{isCameraOpen ? (
@@ -35,30 +39,41 @@ export default function AddNewEntry({ navigation }) {
 				/>
 			) : (
 				<SafeAreaView>
-					<Text style={styles.heading}>New Entry</Text>
-					<View style={styles.container}>
-						<Text style={styles.text}>Enter A Title:</Text>
-						<TextInput style={styles.title} placeholder="Journal Title..." />
-						<Text style={styles.text}>Describe your day:</Text>
-						<TextInput
-							style={styles.textarea}
-							placeholder="Description of journal..."
-						/>
-						<CustomButton onPress={() => {
-								handleOpenCamera();
+					<ScrollView>
+						<Text style={styles.heading}>New Entry</Text>
+						<View style={styles.container}>
+							<Text style={styles.text}>Enter A Title:</Text>
+							<TextInput style={styles.title} placeholder="Journal Title..." />
+							<Text style={styles.text}>Describe your day:</Text>
+							<TextInput
+								style={styles.textarea}
+								placeholder="Description of journal..."
+							/>
+							<CustomButton
+								onPress={() => {
+									handleOpenCamera();
+								}}
+								title="Take a Picture"
+							/>
+							<CustomButton
+								onPress={() => {
+									handleGetLocation();
+								}}
+								title="Take a Picture"
+							/>
+						</View>
+						<CustomButton
+							onPress={() => {
+								console.log('submitting journal');
 							}}
-							title="Take a Picture" />
-					</View>
-					<CustomButton onPress={() => {
-							console.log('submitting journal');
-				}} title="Submit Journal" />
+							title="Submit Journal"
+						/>
+					</ScrollView>
 				</SafeAreaView>
 			)}
 		</SafeAreaView>
 	);
 }
-
-
 
 const styles = StyleSheet.create({
 	container: {
@@ -71,17 +86,17 @@ const styles = StyleSheet.create({
 		marginTop: 1,
 		margin: 20,
 		alignContent: 'center',
-    borderWidth: 0.2,
-		borderBottomColor:'#5c374c',
-		borderBottomWidth:8,
+		borderWidth: 0.2,
+		borderBottomColor: '#5c374c',
+		borderBottomWidth: 8,
 	},
-	heading:{
-		paddingTop:0,
-    color: '#5c374c',
-    fontSize: 35,
-    lineHeight: 84,
-    fontWeight: 'bold',
-		marginLeft: 10
+	heading: {
+		paddingTop: 0,
+		color: '#5c374c',
+		fontSize: 35,
+		lineHeight: 84,
+		fontWeight: 'bold',
+		marginLeft: 10,
 	},
 	title: {
 		height: 50,
@@ -95,10 +110,9 @@ const styles = StyleSheet.create({
 		margin: 20,
 		backgroundColor: '#FFF',
 	},
-	text:{
+	text: {
 		color: '#5c374c',
 		marginLeft: 18,
 		paddingTop: 25,
-		
-	}
+	},
 });
