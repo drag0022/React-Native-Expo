428afd13fb5e0809419a2b5b88f2f0a60858f6e9
onib0002 <onib0002@algonquinlive.com>
2021-12-13 21:58:18 -0500

    journal entries page styled and other files modified

--- screens/JournalEntries.js
+++ screens/JournalEntries.js
@@ -7,6 +7,7 @@ import {
 	FlatList,
 	Image,
 } from 'react-native';
+import { Card } from 'react-native-elements';
 
 export default function JournalEntries({ data, setData }) {
 	console.log({ data });
@@ -16,33 +17,44 @@ export default function JournalEntries({ data, setData }) {
 				data={data}
 				renderItem={(item) => (
 					<View>
-						<Text>ID: {item.item.id ? item.item.id : 'no id available'}</Text>
-						<Text>
-							Title: {item.item.title ? item.item.title : 'no title available'}
-						</Text>
-						<Text>
-							Body: {item.item.body ? item.item.body : 'no body available'}
-						</Text>
-						{item.item.location ? (
-							<Text>
-								Location: Latitude: {item.item.location.coords.latitude}{' '}
-								Longitude: {item.item.location.coords.longitude} Speed:{' '}
-								{item.item.location.coords.speed}
+						<Card containerStyle={{
+							backgroundColor: '#ffffff', shadowColor: "rgba(0, 0, 0, .5)",
+							shadowOffset: { width: 4, height: 8 },
+							shadowOpacity: 0.25,
+							alignSelf:'center',
+							shadowRadius: 12,
+							borderBottomColor: '#A10582',
+							borderBottomWidth: 8,
+							borderRadius:10}}
+						>
+							<Card.Title style={{color: '#A10582'}}>Title: {item.item.title ? item.item.title : 'no title available'}</Card.Title>				
+							<Card.Divider/>	
+							{item.item.image ? (
+								<Card.Image		
+                  resizeMode="cover"
+									style={styles.image}
+									source={{
+										uri: `${item.item.image}`,
+									}}
+								/>
+							) : (
+								<Text style={styles.text}>No Image Available</Text>
+							)}
+			
+							<Text style={styles.text}>ID: {item.item.id ? item.item.id : 'no id available'}</Text>
+							<Text style={styles.text}>
+								Body: {item.item.body ? item.item.body : 'no body available'}
 							</Text>
-						) : (
-							<Text>no location information available</Text>
-						)}
-
-						{item.item.image ? (
-							<Image
-								style={styles.image}
-								source={{
-									uri: `${item.item.image}`,
-								}}
-							/>
-						) : (
-							<Text>No Image Available</Text>
-						)}
+							{item.item.location ? (
+								<Text style={styles.text}>
+									Location: Latitude: {item.item.location.coords.latitude}{' '}
+									Longitude: {item.item.location.coords.longitude} Speed:{' '}
+									{item.item.location.coords.speed}
+								</Text>
+							) : (
+								<Text style={styles.text}>no location information available</Text>
+							)}
+						</Card>
 					</View>
 				)}
 				keyExtractor={(item) => item.id}
@@ -52,8 +64,15 @@ export default function JournalEntries({ data, setData }) {
 }
 
 const styles = StyleSheet.create({
+	text:{
+		color:'#A10582',
+		marginTop: 5,
+	},
+
 	image: {
-		width: 100,
-		height: 100,
+		width: 220,
+		height: 200,
+		borderRadius:10,
+		alignSelf:'center'
 	},
 });
