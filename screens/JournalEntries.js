7f476393f4efa37f4527a3d5d716f5a336e856a6
Eduard Draghiciu <eduarddr96@gmail.com>
2021-12-13 22:31:17 -0500

    fix conflicts

--- screens/JournalEntries.js
+++ screens/JournalEntries.js
@@ -17,22 +17,27 @@ export default function JournalEntries({ data, setData }) {
 				data={data}
 				renderItem={(item) => (
 					<View>
-
-						<Card containerStyle={{
-							backgroundColor: '#ffffff', shadowColor: "rgba(0, 0, 0, .5)",
-							shadowOffset: { width: 4, height: 8 },
-							shadowOpacity: 0.25,
-							alignSelf:'center',
-							shadowRadius: 12,
-							borderBottomColor: '#A10582',
-							borderBottomWidth: 8,
-							borderRadius:10}}
+						<Card
+							containerStyle={{
+								backgroundColor: '#ffffff',
+								shadowColor: 'rgba(0, 0, 0, .5)',
+								shadowOffset: { width: 4, height: 8 },
+								shadowOpacity: 0.25,
+								alignSelf: 'center',
+								shadowRadius: 12,
+								borderBottomColor: '#A10582',
+								borderBottomWidth: 8,
+								borderRadius: 10,
+							}}
 						>
-							<Card.Title style={{color: '#A10582'}}>Title: {item.item.title ? item.item.title : 'no title available'}</Card.Title>				
-							<Card.Divider/>	
+							<Card.Title style={{ color: '#A10582' }}>
+								Title:{' '}
+								{item.item.title ? item.item.title : 'no title available'}
+							</Card.Title>
+							<Card.Divider />
 							{item.item.image ? (
-								<Card.Image		
-                  resizeMode="cover"
+								<Card.Image
+									resizeMode="cover"
 									style={styles.image}
 									source={{
 										uri: `${item.item.image}`,
@@ -41,27 +46,23 @@ export default function JournalEntries({ data, setData }) {
 							) : (
 								<Text style={styles.text}>No Image Available</Text>
 							)}
-			
-							<Text style={styles.text}>ID: {item.item.id ? item.item.id : 'no id available'}</Text>
+
+							<Text style={styles.text}>
+								ID: {item.item.id ? item.item.id : 'no id available'}
+							</Text>
 							<Text style={styles.text}>
 								Body: {item.item.body ? item.item.body : 'no body available'}
-
-						<Text>ID: {item.item.id ? item.item.id : 'no id available'}</Text>
-						<Text>
-							Title: {item.item.title ? item.item.title : 'no title available'}
-						</Text>
-						<Text>
-							Body: {item.item.body ? item.item.body : 'no body available'}
-						</Text>
-						<Text>
-							City: {item.item.city ? item.item.city : 'no city available'}
-						</Text>
-						{item.item.location ? (
+							</Text>
+							<Text>ID: {item.item.id ? item.item.id : 'no id available'}</Text>
 							<Text>
-								Location: Latitude: {item.item.location.coords.latitude}{' '}
-								Longitude: {item.item.location.coords.longitude} Speed:{' '}
-								{item.item.location.coords.speed}
- 
+								Title:{' '}
+								{item.item.title ? item.item.title : 'no title available'}
+							</Text>
+							<Text>
+								Body: {item.item.body ? item.item.body : 'no body available'}
+							</Text>
+							<Text>
+								City: {item.item.city ? item.item.city : 'no city available'}
 							</Text>
 							{item.item.location ? (
 								<Text style={styles.text}>
@@ -70,7 +71,9 @@ export default function JournalEntries({ data, setData }) {
 									{item.item.location.coords.speed}
 								</Text>
 							) : (
-								<Text style={styles.text}>no location information available</Text>
+								<Text style={styles.text}>
+									no location information available
+								</Text>
 							)}
 						</Card>
 					</View>
@@ -82,15 +85,15 @@ export default function JournalEntries({ data, setData }) {
 }
 
 const styles = StyleSheet.create({
-	text:{
-		color:'#A10582',
+	text: {
+		color: '#A10582',
 		marginTop: 5,
 	},
 
 	image: {
 		width: 220,
 		height: 200,
-		borderRadius:10,
-		alignSelf:'center'
+		borderRadius: 10,
+		alignSelf: 'center',
 	},
 });
